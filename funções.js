66a847fdfd9a7a9ee35a14da00710c3871732756
pqnin23 <128159590+pqnin23@users.noreply.github.com>
2024-03-15 08:46:34 -0300

    Add files via upload

---
+++ funções.js
@@ -0,0 +1,25 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// DECLARAÇÃO DE FUNÇÃO
+
+// 1) declara a função
+                      //string
+
+//function imprimeTexto(texeto) {    consol.log(texto); }
+
+ 
+
+  // 2) executa a função (1 ou + vezes)
+
+ // imprimeTexto("oi");
+ // imprimeTexto("outro texto");
+
+  // três formas de escrever funções
+
+  function soma(){
+    return = 2 + 2;
+  }
+
+  console.log(soma())
